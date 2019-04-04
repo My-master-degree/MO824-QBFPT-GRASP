0acd4fda630a7b5947e4a317cc012cf51f08e5e2
Matheus Diógenes Andrade <matheusdiogenesandrade@gmail.com>
2019-04-04 18:20:27 -0300

    Implementação do GRASP para para o QBFPT

--- src/problems/qbf/solvers/GRASP_QBFPT.java
+++ src/problems/qbf/solvers/GRASP_QBFPT.java
@@ -1,7 +1,12 @@
 package problems.qbf.solvers;
 
+import java.awt.List;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 import metaheuristics.grasp.AbstractGRASP;
 import problems.qbf.QBFPT_Inverse;
@@ -19,7 +24,7 @@ import solutions.Solution;
  * @author ccavellucci, fusberti
  */
 public class GRASP_QBFPT extends AbstractGRASP<Integer> {
-
+	
 	/**
 	 * Constructor for the GRASP_QBF class. An inverse QBF objective function is
 	 * passed as argument for the superclass constructor.
@@ -78,10 +83,46 @@ public class GRASP_QBFPT extends AbstractGRASP<Integer> {
 	 */
 	@Override
 	public void updateCL() {
-		
-		QBFPT_Inverse QBFPT_Inverse = (QBFPT_Inverse) this.ObjFunction;
+		QBFPT_Inverse QBFPT_Inverse = (QBFPT_Inverse) super.ObjFunction;
 		Integer[][] triples = QBFPT_Inverse.triples;
-//		super.CL = new ArrayList<Integer>();
+//		mount filled triples
+		Map<Integer[], Integer> filledTriples = new HashMap<Integer[], Integer> ();			
+		for (int i = 0; i < super.incumbentSol.size(); i++) {
+			Integer item = super.incumbentSol.get(i);
+			for (int j = 0; j < triples.length; j++) {				
+				Integer tripleCount = filledTriples.get(triples[j]);						
+				Boolean tripleCountNull = tripleCount == null,
+						candidateMatch = triples[j][0].equals(item) || triples[j][1].equals(item) || triples[j][2].equals(item);
+//				System.out.println("\tTriple ("+triples[j][0]+", "+triples[j][1]+","+triples[j][2]+")");
+				if (candidateMatch) 
+					if (tripleCountNull)
+						filledTriples.put(triples[j], 1);
+					else if (tripleCount == 1) 
+						filledTriples.put(triples[j], 2);
+					else
+						break;
+//					System.out.println("\tCandidate "+item+" passed in triple ("+triples[j][0]+", "+triples[j][1]+","+triples[j][2]+")");													
+			}
+		}
+//		mount cl
+		ArrayList<Integer> oldCL = super.CL; 
+		super.CL = new ArrayList<Integer>();
+		for (int i = 0; i < oldCL.size(); i++) {
+			Boolean candidateIn = true;
+			Integer item = oldCL.get(i);
+			for (Integer[] triple : filledTriples.keySet()) {
+				Integer tripleCount = filledTriples.get(triple);
+				if (tripleCount != null && tripleCount == 2 && (triple[0].equals(item) || triple[1].equals(item) || triple[2].equals(item))) {
+//					System.out.println("\tCandidate "+item+" break in triple ("+triple[0]+", "+triple[1]+","+triple[2]+") that already has "+tripleCount);
+					candidateIn = false;
+					break;
+				}
+			}
+			if (candidateIn)
+				super.CL.add(item);
+		}
+//		System.out.println(super.CL);
+		
 		
 	}
 
@@ -170,11 +211,52 @@ public class GRASP_QBFPT extends AbstractGRASP<Integer> {
 		long startTime = System.currentTimeMillis();
 		GRASP_QBFPT grasp = new GRASP_QBFPT(0.05, 1000, "instances/qbf020");
 		Solution<Integer> bestSol = grasp.solve();
+		
+		
+		QBFPT_Inverse QBFPT_Inverse = (QBFPT_Inverse) grasp.ObjFunction;
+		Integer[][] triples = QBFPT_Inverse.triples;
+		for (int i = 0; i < triples.length; i++) {
+			for (int j = 0; j < triples[i].length; j++) {
+				System.out.print(triples[i][j]+", ");
+			}
+			System.out.println();
+		}
+		
+		
+		
+		
 		System.out.println("maxVal = " + bestSol);
 		long endTime   = System.currentTimeMillis();
 		long totalTime = endTime - startTime;
-		System.out.println("Time = "+(double)totalTime/(double)1000+" seg");		
-
+		System.out.println("Time = "+(double)totalTime/(double)1000+" seg");				
+		
+		
+				
+//		Map<Integer[], Integer> filledTriples = new HashMap<Integer[], Integer> ();			
+//		for (int i = 0; i < grasp.incumbentSol.size(); i++) {
+//			Integer item = grasp.incumbentSol.get(i);
+//			for (int j = 0; j < triples.length; j++) {				
+//				Integer tripleCount = filledTriples.get(triples[j]);						
+//				Boolean tripleCountNull = tripleCount == null,
+//						candidateMatch = triples[j][0].equals(item) || triples[j][1].equals(item) || triples[j][2].equals(item);
+////				System.out.println("\tTriple ("+triples[j][0]+", "+triples[j][1]+","+triples[j][2]+")");
+//				if (candidateMatch) 
+//					if (tripleCountNull)
+//						filledTriples.put(triples[j], 1);
+//					else if (tripleCount == 1) 
+//						filledTriples.put(triples[j], 2);
+//					else
+//						break;
+////					System.out.println("\tCandidate "+item+" passed in triple ("+triples[j][0]+", "+triples[j][1]+","+triples[j][2]+")");													
+//			}
+//		}
+//		
+//		for (Integer[] triple : filledTriples.keySet()) {
+//			Integer tripleCount = filledTriples.get(triple);
+//			if (tripleCount != null && tripleCount == 2 && (triple[0].equals(item) || triple[1].equals(item) || triple[2].equals(item))) {
+//				System.out.println("\tCandidate "+item+" break in triple ("+triple[0]+", "+triple[1]+","+triple[2]+") that already has "+tripleCount);
+//			}
+//		}
 	}
 
 }
